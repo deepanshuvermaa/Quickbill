580f97383d9cb899721653f6ef5b78ef2370a6ef
deepanshuvermaa <deepanshuverma966@gmail.com>
2025-06-17 16:52:51 +0530

    Remove native directories and update EAS config

--- app.config.js
+++ app.config.js
@@ -1,9 +1,18 @@
 module.exports = ({ config }) => {
   return {
     ...config,
+    name: "QuickBill - POS & Billing",
+    slug: "quickbill-pos",
+    version: "1.0.0",
     android: {
       ...config.android,
-      package: "com.quickbill.pos"
+      package: "com.quickbill.pos",
+      versionCode: 1
+    },
+    ios: {
+      ...config.ios,
+      bundleIdentifier: "com.quickbill.pos",
+      buildNumber: "1"
     }
   };
 };
